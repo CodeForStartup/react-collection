44b812f2ac483f130e873e7cceecd9f424cfd4ea
Luan Nguyen <luannguyenbkit@gmail.com>
2024-05-14 22:53:39 +0700

    Update hook form

--- app/yup-form/YupForm.tsx
+++ app/yup-form/YupForm.tsx
@@ -2,12 +2,70 @@
 
 import React from "react";
 
-import { Form, FormDescription, FormField } from "@components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@components/ui/form";
 import { useForm } from "react-hook-form";
 import { Input } from "@/components/ui/input";
 
+enum Countries {
+  US = "United States",
+  CA = "Canada",
+  UK = "United Kingdom",
+  AU = "Australia",
+  DE = "Germany",
+  FR = "France",
+  JP = "Japan",
+  CN = "China",
+  KR = "South Korea",
+  IN = "India",
+  BR = "Brazil",
+  MX = "Mexico",
+  RU = "Russia",
+  ZA = "South Africa",
+  NG = "Nigeria",
+  EG = "Egypt",
+  KE = "Kenya",
+  SA = "Saudi Arabia",
+  AE = "United Arab",
+  VI = "Vietnam",
+}
+
+type UserForm = {
+  first_name: string;
+  last_name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+  profileImage?: File;
+  bio?: string;
+  country: Countries;
+  overtime?: boolean;
+  experiences: Array<{
+    title: string;
+    company: string;
+    location?: string;
+  }>;
+  skills: Array<string>;
+  projects: Array<{
+    title: string;
+    description: string;
+    url?: string;
+  }>;
+};
+
 const YupForm = () => {
-  const form = useForm();
+  const form = useForm<UserForm>({
+    defaultValues: {
+      first_name: "",
+    },
+  });
 
   const { handleSubmit } = form;
 
@@ -16,15 +74,29 @@ const YupForm = () => {
   };
 
   return (
-    <Form {...form}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <FormDescription>Form description...</FormDescription>
-        <FormField
-          name="name"
-          render={({ field }) => <Input {...field} placeholder="Name" />}
-        />
-      </form>
-    </Form>
+    <div className="w-full max-w-[800px] m-auto py-20">
+      <Form {...form}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormField
+            name="first_name"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>First name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="First name" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Validate: required, max-length: 30, includes [a-zA-Z\s]
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+        </form>
+      </Form>
+    </div>
   );
 };
 
--- app/yup-form/page.tsx
+++ app/yup-form/page.tsx
@@ -3,8 +3,8 @@ import YupForm from "./YupForm";
 
 const Page = () => {
   return (
-    <div className="container">
-      <div className="mt-10">
+    <div className="container p-20">
+      <div className="m-auto">
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
           React Hook Form with Yup Validation
         </h1>
